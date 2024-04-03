29c941f79f1ba5fbdce60bdd5c4acefc7fc2bf20
Swati jagtap <swatijagtap5060@gmail.com>
2024-04-03 18:26:50 +0530

    submitted

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,43 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import axios from 'axios'
 
 const App = () => {
+  const [search, setSearch] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  function handleSearch() {
+    axios.get(`https://www.omdbapi.com/?s=${search}&apikey=61e718e9`)
+      .then((res) => {
+        console.log(res.data.Search);
+        setMovies(Object.values(res.data.Search));
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} />
+      <button onClick={handleSearch}>Search</button>
+
+      <ul>
+        {movies && movies.length > 0 ? (
+          movies.map((movie) => (
+            <li key={movie.imdbID}>
+              <h2>
+                {movie.Title} ({movie.Year})
+              </h2>
+              <img src={movie.Poster} alt={movie.Title} />
+            </li>
+          ))
+        ) : (
+          <div className='error'>Invalid movie name. Please try again.</div>
+        )}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
